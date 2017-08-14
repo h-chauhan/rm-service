66b90dd8750f939738d70e1f084e196392477b1f
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-14 13:24:45 +0530

    Allowed-hosts

--- rmbot/settings.py
+++ rmbot/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 's!^*d(ewrvxuepw!b6f37tfx8!v-(^+z7q*3f@n1*68347ba)9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "https://rmbot-service.herokuapp.com/",
+]
 
 
 # Application definition
