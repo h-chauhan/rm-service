4b67f5dca14cbd7214c72d7b95c0329f7559a8ea
Himanshu Chauhan <hchauhan1404@outlook.com>
2018-08-11 21:14:27 +0530

    fix bug

--- api/views.py
+++ api/views.py
@@ -3,24 +3,27 @@ from django.http import HttpResponse, JsonResponse
 
 from .scrape import *
 
-iaccount = Account.getAccount("Internship")
-ibrowser = login("Internship", iaccount)
-paccount = Account.getAccount("Placement")
-pbrowser = login("Placement", paccount)
-
 # Create your views here.
 def getInternNotifications(request):
+    iaccount = Account.getAccount("Internship")
+    ibrowser = login("Internship", iaccount)
     result = parseNotifications("Internship", ibrowser)
     return JsonResponse(result, safe=False)
 
 def getNotifications(request):
+    paccount = Account.getAccount("Placement")
+    pbrowser = login("Placement", paccount)
     result = parseNotifications("Placement", pbrowser)
     return JsonResponse(result, safe=False)
 
 def getInternJobs(request):
+    iaccount = Account.getAccount("Internship")
+    ibrowser = login("Internship", iaccount)
     result = parseJobs("Internship", ibrowser)
     return JsonResponse(result, safe=False)
 
 def getJobs(request):
+    paccount = Account.getAccount("Placement")
+    pbrowser = login("Placement", paccount)
     result = parseJobs("Placement", pbrowser)
     return JsonResponse(result, safe=False)
