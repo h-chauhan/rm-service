ddf6c2f9e8aaa31873c0e4c3d0fc7a94dc7a660f
Himanshu Chauhan <hchauhan1404@outlook.com>
2018-08-12 12:14:07 +0530

    notifs anf jobs models

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import RMAccount
+from .models import *
 
 # Register your models here.
 admin.site.register(RMAccount)
+admin.site.register(InternNotification)
+admin.site.register(InternJobOpening)
+admin.site.register(PlacementNotification)
+admin.site.register(PlacementJobOpening)
--- api/models.py
+++ api/models.py
@@ -8,3 +8,34 @@ class RMAccount(models.Model):
 
     def __str__(self):
         return self.type + " : " + self.username
+
+class Notification(models.Model):
+    date = models.CharField(max_length=50)
+    time = models.CharField(max_length=50)
+    header = models.CharField(max_length=200)
+    body  = models.TextField()
+    poster = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.date + " : " + self.time
+
+class PlacementNotification(Notification):
+    pass
+    
+class InternNotification(Notification):
+    pass
+
+class JobOpening(models.Model):
+    name = models.CharField(max_length=100)
+    deadline = models.CharField(max_length=100)
+    dateOfVisit = models.CharField(max_length=100)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class PlacementJobOpening(JobOpening):
+    pass
+    
+class InternJobOpening(JobOpening):
+    pass
